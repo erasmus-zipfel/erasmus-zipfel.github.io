442434e520262fedb617f0c8db5d0288de252cc1
erasmus-zipfel <erazm.zipfel@gmx.de>
2020-02-24 20:43:25 +0100

    Add files via upload

---
+++ width.js
@@ -0,0 +1,20 @@
+
+window.addEventListener('resize', setSize);
+
+function setSize() {
+    var w = window.innerWidth;
+    var h = window.innerHeight;
+    if (h < w) {
+        document.getElementById('offs').style.width = (w - h) * 0.5;
+        w = h;
+    }
+    else { 
+        document.getElementById('offs').style.width = 0;
+    }
+    h = w * 0.033;
+    w = w * 0.9;
+    document.getElementById('i1').style.width = h; 
+    document.getElementById('i2').style.width = h;
+    document.getElementById('i3').style.width = w;
+    document.getElementById('i4').style.width = h;
+}
